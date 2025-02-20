fd128fe45a54fbffe9c8f90b2c3f17332ef82bf1
Robert Floor <Robert.Floor@schiphol.nl>
2025-02-20 15:02:54 +0100

    Working method level basic auth

--- src/main/java/net/javaguides/todo/config/SpringSecurityConfig.java
+++ src/main/java/net/javaguides/todo/config/SpringSecurityConfig.java
@@ -27,12 +27,6 @@ public class SpringSecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeHttpRequests(authorize -> {
-//                authorize.requestMatchers(HttpMethod.POST, "/api/**").hasRole(("ADMIN"));
-//                authorize.requestMatchers(HttpMethod.PUT, "/api/**").hasRole(("ADMIN"));
-//                authorize.requestMatchers(HttpMethod.DELETE, "/api/**").hasRole(("ADMIN"));
-//                authorize.requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("USER", "ADMIN");
-//                authorize.requestMatchers(HttpMethod.PATCH, "/api/**").hasAnyRole("USER", "ADMIN");
-//                authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll();
                 authorize.anyRequest().authenticated();
             })
             .httpBasic(Customizer.withDefaults());
